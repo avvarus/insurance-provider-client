64954c93a9b8aca540d06f87f36bb4072be5e8cf
Avvarus <avvaru79.java@mail.com>
2019-09-22 18:22:20 -0400

    101

---
+++ src/main/java/com/example/insuranceproviderclient/InsuranceProviderClientApplication.java
@@ -0,0 +1,55 @@
+package com.example.insuranceproviderclient;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+@RequestMapping(value = "/list")
+
+public class InsuranceProviderClientApplication {
+
+    @Value("${insurance.url}")
+    private String url;
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(InsuranceProviderClientApplication.class, args);
+    }
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping(value = "/new")
+    public ResponseEntity getValue() {
+
+        return getResponseEntity();
+    }
+
+    /**
+     * @return
+     */
+    private ResponseEntity getResponseEntity() {
+
+        return getRestTemplate().
+                exchange(url,
+                        HttpMethod.GET,
+                        null,
+                        new ParameterizedTypeReference<List<String>>() {
+                        });
+    }
+}
